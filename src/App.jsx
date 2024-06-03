089b7156f605fbe3960f125a96d4aa26268494bb
VÍCTOR SILVEIRA RODRIGUES <victor_rodrigues2@estudante.sesisenai.org.br>
2024-06-03 12:20:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React,{ useState, useEffect } from 'react'
+import style from 'styled-components'
+import {motion} from "framer-motion"
+import './App.css'
+
+
+const Spinner = style(motion.div)`
+width:50px;
+height:50px;
+border: 4px solid black;
+border-top: 4px solid red;
+border-radius: 50%;
+display: inline-block;
+`;
+
+const Container =style.div`display:flex;
+width: 100vw;
+align-items: center;
+justify-content: center;
+height:100vh;
+`
+
+
+function App() {
+ const [carregando, setCarregando] = useState(true);
+ useEffect(()=>{
+  const tempo = setTimeout(()=>{
+    setCarregando(false);
+  }, 5000)
+  return () => clearTimeout(tempo);
+ },[])
+
+  return (
+    <>
+     <Container>
+      {carregando ?(
+     <Spinner animate = {{rotate: 360}}
+     transition={{duration: 1, repeat: Infinity, ease: 'linear'}}
+     >
+
+     </Spinner>
+     ):(
+      <h1>Pagina carregada</h1>
+     )} 
+     </Container>
+    </>
+  )
+}
+
+export default App
